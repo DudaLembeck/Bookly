68123cb92d402ad1f840a549201aeaaac61893fe
Maria Lembeck <mariaeduardalembeck36@gmail.com>
2026-04-28 16:54:02 -0300

    LoginPage

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -24,17 +24,17 @@
             <ion-card-subtitle>Realize seu Login </ion-card-subtitle>
           </ion-card-header>  
            <ion-card-content>
-        <div v-if="Login">
            <h2 >Email:</h2>
            <ion-input  v-model="email" type="text" placeholder="Insira seu email"></ion-input>
              <h2>Senha:</h2>
            <ion-input  v-model="senha" type="password" placeholder="Digite sua senha"></ion-input>
            <a @click="$router.push('/recuperar')">Esqueceu sua senha?</a>
 
-           <ion-button  @click="entrar" expand="block" fill="clear" shape="round">
+           <ion-button  v-if="email && senha"
+  @click="$router.push('/home')" expand="block" fill="clear" shape="round">
             ENTRAR
            </ion-button>
-              </div>
+         
         </ion-card-content>
     
       </ion-card>
@@ -50,36 +50,11 @@ import { IonContent,IonInput, IonHeader, IonPage, IonTitle, IonToolbar,IonCard, 
 
 
 import { ref } from 'vue'
-import { computed } from 'vue'
-
-const usuarioValido = computed(() => {
-  return Login.some(user => 
-    user.email === email.value && user.senha === senha.value
-  )
-})
-
-const entrar = () => {
-  if (usuarioValido.value) {
-    // login correto
-    window.location.href = '/home'
-  } else {
-    alert('Email ou senha inválidos')
-  }
-}
 
 const email = ref('')
 const senha = ref('')
 
-const Login = [ 
-    { 
-        email: "marialembeck@gmail.com", 
-        senha: "123" 
-    }, 
-    { 
-        email: "karinameier@gmail.com",
-        senha: "123" 
-    } 
-]
+
 
 </script>
 
